fd6f5525433c05d22ba90dc62f347fd7085be181
cameronsmorgan <cameronsmorgan514@gmail.com>
2024-09-30 08:12:32 +0200

    Update

--- Scripts/menu.js
+++ Scripts/menu.js
@@ -7,7 +7,7 @@ const menuItems = [
     {name: "Design", href:`${root}/Design/index.html`},
     {name: "Theory", href:`${root}/Theory/index.html`},
     {name: "DataViz", href:`${root}/DataViz/index.html`},
-    {name: "History", href:`${root}/History/history.html`},
+    {name: "Memories", href:`${root}/Memories/memories.html`},
 ];
 
 export function initialise(currentPage) {
